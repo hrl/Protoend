91cd83a95b348762cc4d1f1ab89177c0852be2ee
hrl <cxghrl@gmail.com>
2015-10-25 04:54:05 +0800

    main run: fix PEP8

--- run.py
+++ run.py
@@ -1,29 +1,30 @@
 from handlers import *
 import tornado.web
 import tornado.ioloop
-#@from tornado import autoreload
+# from tornado import autoreload
 import os
 
-TEMPLATEPATH = os.path.join(os.path.dirname(__file__),'templatepath')
-STATICPATH = os.path.join(os.path.dirname(__file__),'static')
+TEMPLATEPATH = os.path.join(os.path.dirname(__file__), 'templatepath')
+STATICPATH = os.path.join(os.path.dirname(__file__), 'static')
 
-class Application( tornado.web.Application  ):
+
+class Application(tornado.web.Application):
     def __init__(self):
         handlers = [
-            ('/',IndexHandler),
+            ('/', IndexHandler),
             (r"/static", tornado.web.StaticFileHandler,
              dict(path=STATICPATH)),
         ]
 
         settings = dict(
-            template_path = TEMPLATEPATH,
-            static_path = STATICPATH,
+            template_path=TEMPLATEPATH,
+            static_path=STATICPATH,
             #default_handler_class=NotFoundHandler,
             gzip=True,
             #@cookie_secret=SECRET_KEY,
             #xsrf_cookies=True
             )
-        tornado.web.Application.__init__(self,handlers,**settings)
+        tornado.web.Application.__init__(self, handlers, **settings)
 
 
 if __name__ == '__main__':
