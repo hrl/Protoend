20aefd7c6a5c66f5e55a0d15998a68d3ca8addac
hrl <cxghrl@gmail.com>
2015-10-25 05:34:53 +0800

    main handler: fix bugs

--- handlers.py
+++ handlers.py
@@ -1,26 +1,12 @@
+import string
+import random
+import os
+import os.path
+
 import tornado.web
 import tornado.gen
-from tornado.escape import json_encode
-import time
-from threading import Thread
+
 from generator import generator
-import os
-
-
-class Worker(Thread):
-    def __init__(self, req):
-        self.req = req
-        self.input_path = os.path.join(os.getcwd(), 'generator/template')
-        self.output_path = os.path.join(os.getcwd(), 'static/files')
-        super(Worker, self).__init__()
-
-    def run(self):
-        # do the function here
-        generator.render_all(self.input_path,
-                             self.output_path,
-                             self.output_path,
-                             self.req.data)
-        self.req.response['path'] = os.path.join('/static/files', 'backend.tar')
 
 
 class BaseHandler(tornado.web.RequestHandler):
@@ -38,18 +24,26 @@ class IndexHandler(BaseHandler):
     def get(self):
         self.redirect('/static/chartPage.html')
 
-
-    #@tornado.web.asynchronous
-    #@tornado.gen.engine
     def post(self):
-        self.data = self.get_body_argument('data', '')
-        self.name = self.get_argument('name', '')
-        self.response = {}
-        if self.data and self.filename:
-            p = Worker(self)
-            p.start()
-            p.join()
-            self.response['static'] = 200
-        else:
-            self.response['static'] = 400
-        self.write(json_encode(self.response))
+        data = self.request.body.decode('utf8')
+
+        base_dir = os.path.dirname(__file__)
+        static_dir = os.path.join(base_dir, 'static/')
+        generator_template_dir = os.path.join(base_dir, 'generator/template.')
+        generator_output_base_dir = os.path.join(static_dir, 'generator/')
+
+        random_value = ''.join(
+            [random.choice(string.ascii_letters) for i in range(12)]
+        )
+        generator_output_dir = os.path.join(generator_output_base_dir,
+                                            random_value)
+        generator_output_tar = os.path.join(generator_output_base_dir,
+                                            random_value + '.tar')
+        os.mkdir(generator_output_dir)
+
+        generator.render_all(generator_template_dir,
+                             generator_output_dir,
+                             generator_output_tar,
+                             data)
+
+        self.redirect('/static/generator/' + random_value + '.tar')
