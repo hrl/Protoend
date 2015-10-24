81d50a1d3cf9acd520c3d2d86d968ef1d5154c7c
hrl <cxghrl@gmail.com>
2015-10-25 02:59:07 +0800

    generator: main func, fin

--- generator/generator.py
+++ generator/generator.py
@@ -1,6 +1,9 @@
-from mako.template import Template
 import os
 import shutil
+import json
+import tarfile
+
+from mako.template import Template
 
 
 def parse_raw_input(data):
@@ -45,43 +48,51 @@ def parse_raw_input(data):
     return data
 
 
-def render_all():
-    pass
+def render_all(input_path, output_path, tar_path, render_data):
+    render_data = parse_raw_input(json.loads(render_data))
+    render_dir(input_path, output_path, render_data)
+
+    tar = tarfile.open(tar_path, 'w')
+    tar.add(output_path, 'backend')
+    tar.close()
 
 
-def render_dir(input, output, render_data=None):
+def render_dir(input_path, output_path, render_data):
+    if output_path.split('.')[-1].lower() == 'mako':
+        # render dir name
+        # tmp handle
+        parent_path = os.path.dirname(output_path)
+        for table in render_data["tables"]:
+            output_sub_path = os.path.join(parent_path, table["name"])
+            render_sub_data = {
+                'table': table
+            }
+            render_dir(input_path, output_sub_path, render_sub_data)
+        return
+
+    # normal dir
+    # mkdir
     try:
-        os.mkdir(output)
+        os.mkdir(output_path)
     except:
-        shutil.rmtree(output)
-        os.mkdir(output)
-    if not os.path.isabs(input):
-        input = os.path.join(os.getcwd(), input)
-    if not os.path.isabs(output):
-        output = os.path.join(os.getcwd(), output)
+        shutil.rmtree(output_path)
+        os.mkdir(output_path)
 
-    if os.path.isdir(output) \
-            and os.path.splitext(output)[-1].lower() == '.mako':
-        # function for dir dirname *.mako
-        os.rmdir(output)
-        print(output)
-    else:
-        for item in os.listdir(input):
-            # input path
-            path = os.path.join(input, item)
-            # output path
-            outpath = os.path.join(output, item)
-            if os.path.isdir(path):
-                render_dir(path, outpath)
-            elif os.path.isfile(path) \
-                    and os.path.splitext(path)[-1].lower() == '.mako':
-                # function for file *.mako
-                pass
-            else:
-                shutil.copyfile(path, outpath)
+    # render sub dir
+    for item in os.listdir(input_path):
+        input_sub_path = os.path.join(input_path, item)
+        output_sub_path = os.path.join(output_path, item)
+        if os.path.isdir(input_sub_path):
+            render_dir(input_sub_path, output_sub_path, render_data)
+        elif output_sub_path.split('.')[-1].lower() == 'mako':
+            render_file(input_sub_path, output_sub_path[:-5], render_data)
+        else:
+            shutil.copyfile(input_sub_path, output_sub_path)
 
 
 def render_file(template_path, target_path, data):
+    print("render: " + template_path)
+    print("    to: " + target_path)
     file_template = open(template_path, 'r')
     data_template = file_template.read()
     file_template.close()
